36d3fe18878d82c9d28da2bc1463a75052c641ee
muhammad-danish1998 <iamdanish1998@gmail.com>
2020-11-27 00:39:33 +0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,170 @@
+export const placeInfo = [
+  {
+    id: 1,
+    headerTitle: 'BUSINESS AND PROFESSIONAL DEGREES',
+    headerSubTitle: 'Examples such as Accounting and Finance',
+    headerText:
+      'For the aspiring entrepreneurs and business leaders, business and professional management degrees are consistently a popular choice. They provide the academic knowledge and skills to pursue global career opportunities and helps you develop a broad understanding of businesses and specific areas such as finance and human resources.',
+    title: 'WHY STUDY A BUSINESS AND PROFESSIONAL DEGREE?',
+    description:
+      'Business and Professional degrees are a great way to develop many of the transferable skills needed for working in a business environment. There are a number of business and professional degrees which you can undertake, one of the most common of which is an undergraduate business studies degree. Often a business studies degree will cover subjects such as accounting, finance, management and, increasingly, entrepreneurship. You can also do a specific course that covers your interests such as accounting. These wide range of subjects reflects the multidisciplinary nature of business, and the importance of understanding all the key elements needed to make a business successful.',
+    img: '.././images/business.jpg',
+    maps:
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1123.3850145412064!2d37.600589158335865!3d55.72774864513666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54b0be3ac9d11%3A0x735806d25b3bdc55!2z0JzRg9C30LXQuSDRgdC-0LLRgNC10LzQtdC90L3QvtCz0L4g0LjRgdC60YPRgdGB0YLQstCwIMKr0JPQsNGA0LDQtsK7!5e0!3m2!1sru!2sru!4v1547763408711'
+  },
+  {
+    id: 2,
+    headerTitle: 'SOCIAL SCIENCE DEGREES',
+    headerSubTitle: 'Examples such as Criminology, Sociology and English Literature',
+    headerText:
+      'Social Science degrees offer a fascinating insight into everyday life, while also preparing you for the challenges and opportunities that arise in contemporary society. Given the academic focus on societal issues and the relationships of individuals within society, your degree will be critical to challenging social problems and real-life world issues.',
+    title: 'WHY STUDY A SOCIAL SCIENCE DEGREE?',
+    description:
+      'Social sciences Degrees cover a lot of ground. The appeal of all of the courses offered by us at UCP is broad because the many subjects that come under the social sciences umbrella provide fascinating insights into everyday life in our communities, families and workplaces. All social science courses help students develop a wide range of transferable skills and open up many career options. The successful completion of a degree in any social sciences subject demonstrates research skills, analytical ability, a flexible and adaptable approach to learning, critical evaluation, the ability to work to deadlines, creativity, logical thinking, team-working, self-organisation and communication skills – all skills that will serve graduates no matter where they want to take their careers.',
+    img: '.././images/socialsciences.jpg',
+    maps:
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1530.8494832689219!2d37.59693065124095!3d55.74157162835216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54baea59877dd%3A0xb4d0c3eb557a1c98!2zwqvQnNCe0KHQmtCe0JLQodCa0JjQmSDQlNCe0Jwg0KTQntCi0J7Qk9Cg0JDQpNCY0JjCuw!5e0!3m2!1sru!2sru!4v1547845790316'
+  },
+  {
+    id: 3,
+    headerTitle: 'CREATIVE INDUSTRIES DEGREES',
+    headerSubTitle: 'Examples include Digital Arts, Journalism and Media Production',
+    headerText:
+      'Creative Industry Degrees will make you understand how to create value from your ideas; communicate your passion for media and culture; think critically, creatively, ethically and strategically about cultural values and media communications. You will learn how to plan, buy, research, develop, distribute, produce and commission creative work; as well as understand the industrial, political, technological and social issues that underpin the creative process.',
+    title:
+      'WHY STUDY A CREATIVE INDUSTRY DEGREE?',
+    description:
+      'Creative Industy Degrees allow the need to demonstrate a practical creative talent or to show a passion for art, design, music or other creative pursuits, it also allows you to spend time practicing and honing their craft, particularly for actors, dancers and musicians. It also allows you to develop your needs to be independent, proactive and resilient. By doing a creative Industry degree there is a higher than average likelihood of being self-employed or freelance, or of working on short-term contracts, the majority of work is to be project-based and deadline-driven. The working environments to range from offices and art/design/film/photography studios to theatres, museums and music venues, by working hours to vary enormously, from regular office hours to working evenings and weekends, and the flexibility of choosing your hours as a freelancer.',
+    img: '.././images/creativestudies.jpg',
+    maps:
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17967.866965087615!2d37.60335432286895!3d55.74141791432578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb9384e1a3b2da434!2z0JPQvtGB0YPQtNCw0YDRgdGC0LLQtdC90L3Ri9C5INC80YPQt9C10Lkg0JAu0KEuINCf0YPRiNC60LjQvdCw!5e0!3m2!1sru!2sru!4v1547878731704'
+  },
+  {
+    id: 4,
+    headerTitle: 'EDUCATION AND TEACHING DEGREES',
+    headerSubTitle: 'Examples include Early Years and Education, Professional Studies and Education Studies',
+    headerText: 'An education degree will give you a set of key, transferable skills such as an ability to work and communicate with children, effective oral and written communication, information and communication technology savviness, research and analytical skills, problem-solving skills, teamwork ability, self-management, organisation and time management skills.',
+    title: 'WHY STUDY A EDUCATION/TEACHING DEGREE?',
+    description:
+      'Studying an education/teaching at UCP prepares graduates to teach in a variety of settings, usually in a specific subject matter. Depending on your background, interests, and career goals, you might be best suited to an undergraduate or graduate degree in education. This ultimately will result in you being able to proceed in teaching as a career. ',
+    img: '.././images/external.jpg',
+    maps:
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2245.9848620916428!2d37.618675215617316!3d55.741392000339864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54afe32078357%3A0xa50d6e8b9a7b028f!2z0JPQvtGB0YPQtNCw0YDRgdGC0LLQtdC90L3QsNGPINCi0YDQtdGC0YzRj9C60L7QstGB0LrQsNGPINCz0LDQu9C10YDQtdGP!5e0!3m2!1sru!2sru!4v1547878659681'
+  },
+  {
+    id: 5,
+    headerTitle: 'SCIENCE AND TECHNOLOGY DEGREES',
+    headerSubTitle: 'Examples include Biological Sciences, Computing and information systems and Construction',
+    headerText: 'Science and Technology Degrees cover a breadth of disciplines, including biotechnology, life and physical science, animal and plants. Pursuing a degree in Science and Technology Studies at UCP will expose you to key issues of debate regarding the philosophical and social aspects of technological and scientific advancement.',
+    title: 'WHY STUDY A SCIENCE AND TECHNOLOGY DEGREE?',
+    description:
+      'Studying an Technology/Science Degree at UCP will allow you to a degree in a scientific area are usually highly prized. Often, a Science degree will enable graduates to go into well-paid jobs, and even choose the country in which they work.In many countries around the world, STEM graduates are in high demand. Those with scientific expertise are attractive to employers and societies, and often face less restrictive immigration policies. By Having a computing degree, it will provide you with the knowledge, problem-solving skills and logical thinking capabilities that serve as a competitive advantage in your future career. ',
+    img: '.././images/computing.jpg',
+    maps:
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2245.9763498372804!2d37.59654121561725!3d55.741539900328505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54baea59877dd%3A0xb4d0c3eb557a1c98!2zwqvQnNCe0KHQmtCe0JLQodCa0JjQmSDQlNCe0Jwg0KTQntCi0J7Qk9Cg0JDQpNCY0JjCuw!5e0!3m2!1sru!2sru!4v1547878587877'
+  },
+  {
+    id: 6,
+    headerTitle: 'PARTNER COURSES',
+    headerSubTitle: 'Examples include Performing Arts and Sport and Exercise Science',
+    headerText:
+      'Studying Dance provides you with specialist knowledge in different dance forms, dance history, choreography, community dance, arts administration and the commercial and business side of dance and the performing arts. Studying Sports Science combines branches of science, including physiology, psychology, biomechanics and nutrition, with business and management skills to produce graduates who are capable of careers in a range of industries.',
+    title: 'WHY STUDY PARTNER COURSE SUCH AS DANCE AND PERFORMING ARTS?',
+    description:
+      'Studying dance and Performing Arts at UCP develops creativity, teamwork, confidence, critical thinking, self-discipline, physical health and the ability to work collaboratively—all beneficial in any 21st century career path and quality of life. Performing arts are generally either practice-based (involving a great deal of actual performance) or academic, which combines practical work with analysis of the background of the subject. ',
+    img: '.././images/graphicdesign.jpg',
+    maps:
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2244.397087990157!2d37.62463341561838!3d55.76897499821247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54a68e2e342b7%3A0x934bc1e9948c826b!2z0KbQtdC90YLRgCDQnNCw0YDRgQ!5e0!3m2!1sru!2sru!4v1547878491776'
+  }
+];
+
+export const reviews = [
+  {
+    id: 1,
+    name: 'Norbert Bala',
+    avatar: '.././avatars/avatar-1.png',
+    comment:
+      'I have had a very good experience at UCP and cant wait to carry on and succeed in life.'
+  },
+  {
+    id: 2,
+    name: 'Krystian Michalski',
+    avatar: '.././avatars/avatar-2.png',
+    comment:
+      'I really enjoy the supportive staff and having the best lecturers.'
+  },
+  {
+    id: 3,
+    name: 'Mateusz Waldowski',
+    avatar: '.././avatars/avatar-3.png',
+    comment:
+      'I am a Computing student and I can not recommend UCP more to anyone who is intrested.'
+  },
+  {
+    id: 4,
+    name: 'Efeoghene Ogodo',
+    avatar: '.././avatars/avatar-4.png',
+    comment:
+      'UCP is amazing, everyone is lovely and feels like a family'
+  },
+  {
+    id: 5,
+    name: 'Natasha Clements',
+    avatar: '.././avatars/avatar-5.png',
+    comment:
+      'I really enjoy the course and all the feedback from our tutors'
+  }
+];
+
+export const detailInfo = {
+  id: 1,
+  headerTitle: 'COURSES AT UCP',
+  headerSubTitle: 'COME AND JOIN US',
+  headerText:
+    'Any degree demonstrates your ability to think and work independently and you will develop important skills in communication, numeracy and information technology. These are the qualities that many employers look for when recruiting.',
+  title: 'WHY CHOOSE UCP?',
+  description:
+    'At UCP you will learn from lecturers who are highly qualified and experienced in their fields. But great knowledge isn’t the only thing our staff can offer; they are also friendly, supportive and dedicated to helping you achieve your ambitions.',
+  img: '.././images/UCP.jpg',
+  maps:
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1123.3850145412064!2d37.600589158335865!3d55.72774864513666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54b0be3ac9d11%3A0x735806d25b3bdc55!2z0JzRg9C30LXQuSDRgdC-0LLRgNC10LzQtdC90L3QvtCz0L4g0LjRgdC60YPRgdGB0YLQstCwIMKr0JPQsNGA0LDQtsK7!5e0!3m2!1sru!2sru!4v1547763408711'
+};
+
+export const news = [
+  {
+    id: 1,
+    newsTitle: 'lorem ajhhdsad',
+    newsText:
+      'dfdfdfsfesf'
+  },
+  {
+    id: 2,
+    newsTitle: 'efefewfefewfefwe',
+    newsText:
+      'fsfsfefefefe'
+  },
+  {
+    id: 3,
+    newsTitle: 'sffffsfsdfd',
+    newsText:
+      'defefefeffefewffe'
+  },
+  {
+    id: 4,
+    newsTitle: 'Efeoghene Ogodo',
+    newsText:
+      'UCP is amazing, everyone is lovely and feels like a family'
+  },
+  {
+    id: 5,
+    newsTitle: 'Natasha Clements',
+    newsText:
+      'I really enjoy the course and all the feedback from our tutors'
+  },
+  {
+    id: 6,
+    newsTitle: 'Kai Rooke',
+    newsText:
+      'UCP is everyone i .' 
+  }
+];
